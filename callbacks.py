7220fa65ba2e4e1c4f516858d74b366b6e036525
nathanlct <nathan.lichtle@gmail.com>
2022-03-14 15:45:27 +0100

    Log initial and final y in tensorboard

--- callbacks.py
+++ callbacks.py
@@ -1,4 +1,7 @@
 from stable_baselines3.common.callbacks import BaseCallback
+from stable_baselines3.common.logger import Figure
+import matplotlib.pyplot as plt
+from env import WaveEnv
 
 
 class TensorboardCallback(BaseCallback):
@@ -8,12 +11,32 @@ class TensorboardCallback(BaseCallback):
     def __init__(self, verbose=0):
         super(TensorboardCallback, self).__init__(verbose)
 
-    def _on_training_start(self):
-        self.env = self.training_env.envs[0]
-
     def _on_step(self):
-        for infos in self.locals['infos']:
-            for k, v in infos.items():
-                if k not in ['episode', 'terminal_observation']:
-                    self.logger.record(f'custom/{k}', v)
         return True
+    
+    def _on_rollout_end(self):
+        # create test environment
+        test_env = WaveEnv()
+
+        # reset
+        state = test_env.reset()
+
+        # log initial norm of y and plot initial y
+        self.logger.record('custom/init_norm_y', test_env.sim.norm_y())
+        figure = plt.figure()
+        figure.add_subplot().plot(test_env.sim.x, test_env.sim.y)
+        self.logger.record("custom/init_y", Figure(figure, close=True), exclude=("stdout", "log", "json", "csv"))
+        plt.close()
+        
+        # execute environment using trained policy until done
+        done = False
+        while not done:
+            action = self.model.predict(state, deterministic=True)[0]
+            state, reward, done, _ = test_env.step(action)
+
+        # log final norm of y and plot final y
+        self.logger.record('custom/done_norm_y', test_env.sim.norm_y())
+        figure = plt.figure()
+        figure.add_subplot().plot(test_env.sim.x, test_env.sim.y)
+        self.logger.record("custom/done_y", Figure(figure, close=True), exclude=("stdout", "log", "json", "csv"))
+        plt.close()
