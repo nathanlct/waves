5a269e0bef2a378c3899b038efe6a4e7eabb1ee3
nathanlct <nathan.lichtle@gmail.com>
2022-03-15 17:53:26 +0100

    Add max_abs metrics

--- callbacks.py
+++ callbacks.py
@@ -2,6 +2,7 @@ from stable_baselines3.common.callbacks import BaseCallback
 from stable_baselines3.common.logger import Figure
 import matplotlib.pyplot as plt
 from env import WaveEnv
+import numpy as np
 
 
 class TensorboardCallback(BaseCallback):
@@ -23,6 +24,7 @@ class TensorboardCallback(BaseCallback):
 
         # log initial norm of y and plot initial y
         self.logger.record('custom/init_norm_y', test_env.sim.norm_y())
+        self.logger.record('custom/init_max_abs_y', np.max(np.abs(test_env.sim.y)))
         figure = plt.figure()
         figure.add_subplot().plot(test_env.sim.x, test_env.sim.y)
         self.logger.record("custom/init_y", Figure(figure, close=True), exclude=("stdout", "log", "json", "csv"))
@@ -31,6 +33,8 @@ class TensorboardCallback(BaseCallback):
         # execute environment using trained policy until done
         actions = []
         rewards = []
+        y_norms = [test_env.sim.norm_y()]
+        y_max_abs = [np.max(np.abs(test_env.sim.y))]
         times = []
         done = False
         while not done:
@@ -38,10 +42,13 @@ class TensorboardCallback(BaseCallback):
             state, reward, done, _ = test_env.step(action)
             actions.append(action)
             rewards.append(reward)
+            y_norms.append(test_env.sim.norm_y())
+            y_max_abs.append(np.max(np.abs(test_env.sim.y)))
             times.append(test_env.sim.t)
 
         # log final norm of y and plot final y
         self.logger.record('custom/done_norm_y', test_env.sim.norm_y())
+        self.logger.record('custom/done_max_abs_y', np.max(np.abs(test_env.sim.y)))
         figure = plt.figure()
         figure.add_subplot().plot(test_env.sim.x, test_env.sim.y)
         self.logger.record("custom/done_y", Figure(figure, close=True), exclude=("stdout", "log", "json", "csv"))
@@ -56,4 +63,14 @@ class TensorboardCallback(BaseCallback):
         figure = plt.figure()
         figure.add_subplot().plot(times, rewards)
         self.logger.record('custom/rewards', Figure(figure, close=True), exclude=("stdout", "log", "json", "csv"))
+        plt.close()
+
+        figure = plt.figure()
+        figure.add_subplot().plot([0] + times, y_norms)
+        self.logger.record('custom/y_norms', Figure(figure, close=True), exclude=("stdout", "log", "json", "csv"))
+        plt.close()
+
+        figure = plt.figure()
+        figure.add_subplot().plot([0] + times, y_max_abs)
+        self.logger.record('custom/y_max_abs', Figure(figure, close=True), exclude=("stdout", "log", "json", "csv"))
         plt.close()
