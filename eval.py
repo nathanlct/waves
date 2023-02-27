93639b59884391f02bd843ba3bcde20d8149748d
nathanlct <nathan.lichtle@gmail.com>
2023-02-27 13:51:54 -0800

    Script to eval trained checkpoint

--- eval.py
+++ eval.py
@@ -1,19 +1,34 @@
 import numpy as np
 from stable_baselines3 import PPO
+import argparse
+from pathlib import Path
+import json
 
-from env import WaveEnv
+from waves.env import WavesEnv
+from waves.utils import parse_env_args
 
-# load trained model
-model = PPO.load("/Users/hayat/waves/logs/1647360415_15Mar22_17h06m55s/model")
+# parse args
+parser = argparse.ArgumentParser()
+parser.add_argument("cp_path", type=str, help="Path to .zip checkpoint to load and use "
+                    'as a control. There must be a configs.json in the parent directory.')
+args = parser.parse_args()
+
+# load trained model and config file
+model_path = Path(args.cp_path)
+model = PPO.load(str(model_path))
+with open(str(model_path.parent.parent / 'configs.json'), 'r') as f:
+    configs = json.load(f)
+env_kwargs = parse_env_args(configs['args'])
 
 # create env
-env = WaveEnv()
+env = WavesEnv(**env_kwargs)
 
-# evaluation loop
+# eval loop
 obs = env.reset()
-while True:
+done = False
+while not done:
     action, _ = model.predict(obs, deterministic=True)
     obs, reward, done, info = env.step(action)
-    if done:
-        break
-env.sim.render(path="./hardcoded.gif")
+
+# render
+env.sim.render(path=None, display=True, fps=30.0, dpi=100, speed=1.0, no_video=True)
