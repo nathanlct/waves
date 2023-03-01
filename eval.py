c0ff9b463cb40b38820b5150c8f48b9b2ef4291d
ahayat16 <amaury.hayat@gmail.com>
2023-03-01 20:13:19 +0100

    control visualilsation

--- eval.py
+++ eval.py
@@ -2,6 +2,7 @@ import numpy as np
 from stable_baselines3 import PPO
 import argparse
 from pathlib import Path
+from matplotlib import pyplot as plt
 import json
 
 from waves.env import WavesEnv
@@ -9,16 +10,28 @@ from waves.utils import parse_env_args
 
 # parse args
 parser = argparse.ArgumentParser()
-parser.add_argument("cp_path", type=str, help="Path to .zip checkpoint to load and use "
-                    'as a control. There must be a configs.json in the parent directory.')
+parser.add_argument(
+    "cp_path",
+    type=str,
+    help="Path to .zip checkpoint to load and use "
+    "as a control. There must be a configs.json in the parent directory.",
+)
+parser.add_argument(
+    "--plot",
+    type=bool,
+    help="If not False, we plot the control to visualize it rather than using it in an evaluation",
+    default=False,
+)
+
 args = parser.parse_args()
+
 
 # load trained model and config file
 model_path = Path(args.cp_path)
 model = PPO.load(str(model_path))
-with open(str(model_path.parent.parent / 'configs.json'), 'r') as f:
+with open(str(model_path.parent.parent / "configs.json"), "r") as f:
     configs = json.load(f)
-env_kwargs = parse_env_args(configs['args'])
+env_kwargs = parse_env_args(configs["args"])
 
 # create env
 env = WavesEnv(**env_kwargs)
@@ -26,6 +39,22 @@ env = WavesEnv(**env_kwargs)
 # eval loop
 obs = env.reset()
 done = False
+
+
+if args.plot:
+    print("here")
+    x_range = np.linspace(0, 10 * env.sim.K, 1000)
+    control_action = []
+    for val in x_range:
+        action, _ = model.predict(np.array([val]), deterministic=True)
+        action = (action + 1.0) * (
+            env.action_max - env.action_min
+        ) / 2.0 + env.action_min
+        control_action.append(action)
+    plt.plot(x_range, control_action)
+    plt.show()
+    done = True
+
 while not done:
     action, _ = model.predict(obs, deterministic=True)
     obs, reward, done, info = env.step(action)
